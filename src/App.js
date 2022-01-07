ca0a6538fa7b1de2cc2b115f7f57cbae34a31598
collins-nc <noko.nc@mail.com>
2022-01-07 12:57:02 +0200

    initital master

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Blog,Feature,Footer,Header,Possibility,WhatGPT3} from './container';
+import {Brand,CTA,Navbar} from './components';
+import "./index.css";
+import "./App.css";
+
+const App = () => {    
+    return (
+        <div className='App'>
+            <div className='gradient__bg'>
+                <Navbar/>
+                <Header/>
+            </div>
+            <Brand/>
+            <WhatGPT3/>
+            <Feature/>
+            <Possibility/>
+            <CTA/>
+            <Blog/>
+            <Footer/>
+        </div>
+    )
+}
+export default App
---
+++ src/container/possibility/Possibility.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Possibility() {
+    return (
+        <div>
+            Possibility
+        </div>
+    )
+}
+
+export default Possibility
