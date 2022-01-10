18fa95da57269ba1e8fabbd5314139101a1abed8
collins-nc <noko.nc@mail.com>
2022-01-10 16:42:37 +0200

    updated gpt3

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import "./feature.css"
 
-function Feature() {
+function Feature({title,text}) {
     return (
-        <div>
-            Feature
+        <div className='gpt3__features-container__feature'>
+            <div className='gpt3__feature-container__feature-tittle'>
+            <div/>
+            <h1>{title}</h1>
+            </div>
+            <div className='gpt3__feature-container-text'>
+                {text}
+            </div>
+            
         </div>
     )
 }
